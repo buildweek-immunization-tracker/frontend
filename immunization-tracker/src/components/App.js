6074e5680301e8164ef281f54a7d2a3addc82d28
Dave Irwin <david@redirwin.com>
2019-07-29 09:42:46 -0700

    Set up and prepped for project. First commit.

---
+++ immunization-tracker/src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <p>Duis mollit pariatur culpa excepteur sint nisi.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ immunization-tracker/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// styling
+import "./styles/normalize.css";
+import "./styles/index.css";
+
+// custom components
+import App from "./components/App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
