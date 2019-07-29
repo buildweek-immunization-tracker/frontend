0385ea8643ceef98384c82632c2da2844eba28de
Elvis Knapman <elvis@elvisknapman.com>
2019-07-29 19:14:03 -0400

    Added InfoCard

--- immunization-tracker/src/components/App.js
+++ immunization-tracker/src/components/App.js
@@ -1,8 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
+import InfoCard from "./InfoCard";
 // styles
 import "../styles/App.scss";
 
 export default function App() {
-  return <div className="App" />;
+  const [people, setPeople] = useState([
+    { name: "Elvis Knapman", currentProvider: "Current Provider Here" },
+    { name: "Dave Irwin", currentProvider: "Current Provider Here" },
+    { name: "Tatiana Faramarzi", currentProvider: "Current Provider Here" },
+    { name: "Trang Nguyen", currentProvider: "Current Provider Here" },
+    { name: "Van Jordan", currentProvider: "Current Provider Here" }
+  ]);
+  return (
+    <div className="App">
+      <InfoCard people={people} />
+    </div>
+  );
 }
