1e3ce39a0ccb3951fb30ad4ff455d5d460bcf6eb
Trang Nguyen <ttn2127@columbia.edu>
2019-07-30 17:05:27 -0400

    made it so that there is only one status column

--- immunization-tracker/src/components/PatientRecordPage/ShotsRowReceivedAndUpcoming.js
+++ immunization-tracker/src/components/PatientRecordPage/ShotsRowReceivedAndUpcoming.js
@@ -9,10 +9,16 @@ export default function ShotsRow({name, dose, location,dateReceived }){
                 <Table.Cell>{dose}</Table.Cell>
                 <Table.Cell>{location}</Table.Cell>
                 <Table.Cell textAlign='center'><Icon color='green' name='checkmark' size='large' />{dateReceived}</Table.Cell>
-                <Table.Cell textAlign='center'></Table.Cell>
-                <Table.Cell textAlign='center'></Table.Cell>
             </Table.Row>
         )
     }
-    else return <h1>hi</h1>
+    else return(
+
+        <Table.Row>
+            <Table.Cell>{name}</Table.Cell>
+            <Table.Cell>{dose}</Table.Cell>
+            <Table.Cell>{location}</Table.Cell>
+            <Table.Cell textAlign='center'><Icon color='yellow' name='checkmark' size='large' />{dateReceived}</Table.Cell>
+        </Table.Row>
+    )
 }
--- immunization-tracker/src/components/PatientRecordPage/ShotsTable.js
+++ immunization-tracker/src/components/PatientRecordPage/ShotsTable.js
@@ -2,7 +2,7 @@ import React from "react";
 import {useEffect, useState} from "react";
 import Axios from "axios";
 import { Icon, Label, Menu, Table } from 'semantic-ui-react'
-import ShotsRow from "./ShotsRow"
+import ShotsRow from "./ShotsRowReceivedAndUpcoming"
 import styled from "styled-components";
 
 export default function ShotsTable({id}){
@@ -32,12 +32,7 @@ export default function ShotsTable({id}){
                     <Table.HeaderCell rowSpan='2'>Name</Table.HeaderCell>
                     <Table.HeaderCell rowSpan='2'>Dose</Table.HeaderCell>
                     <Table.HeaderCell rowSpan='2'>Provider</Table.HeaderCell>
-                    <Table.HeaderCell colSpan='3'>Status</Table.HeaderCell>
-                </Table.Row>
-                <Table.Row>
-                    <Table.HeaderCell>Administered</Table.HeaderCell>
-                    <Table.HeaderCell>Missed</Table.HeaderCell>
-                    <Table.HeaderCell>Upcoming</Table.HeaderCell>
+                    <Table.HeaderCell colSpan='1'>Status</Table.HeaderCell>
                 </Table.Row>
                 </Table.Header>
             
