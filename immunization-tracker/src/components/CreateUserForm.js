09fd1206cd3e86123b6c881edfda737ce36fd5bd
Dave Irwin <david@redirwin.com>
2019-07-29 17:22:42 -0700

    User creation form completed. Successfully creating new user at endpoint.

--- immunization-tracker/src/components/CreateUserForm.js
+++ immunization-tracker/src/components/CreateUserForm.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
+import axios from "axios";
 
 function CreateUserForm({ touched, errors }) {
   return (
@@ -24,8 +25,13 @@ function CreateUserForm({ touched, errors }) {
           placeholder="Password"
           autoComplete="off"
         />
+        <Field component="select" name="role">
+          <option value="" label="User Type" />
+          <option value="parent">Parent</option>
+          <option value="staff">Healthcare Provider</option>
+        </Field>
 
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </Form>
       <div className="error-group">
         <div className="error-message">
@@ -35,6 +41,7 @@ function CreateUserForm({ touched, errors }) {
         <div className="error-message">
           {touched.password && errors.password}
         </div>
+        <div className="error-message">{touched.role && errors.role}</div>
       </div>
     </section>
   );
@@ -44,7 +51,8 @@ export default withFormik({
     return {
       username: "",
       email: "",
-      password: ""
+      password: "",
+      role: ""
     };
   },
 
@@ -57,11 +65,25 @@ export default withFormik({
       .email("Email is not valid."),
     password: Yup.string()
       .required("Please enter a password.")
-      .min(8, "Password must be at least 8 characters.")
+      .min(8, "Password must be at least 8 characters."),
+    role: Yup.string()
+      .required("Please choose a user type.")
+      .min(3, "Please chose a user type.")
   }),
 
   handleSubmit(values, formikBag) {
     console.log(values);
     formikBag.resetForm();
+    axios
+      .post(
+        "https://immunization-tracker-van.herokuapp.com/api/auth/register",
+        values
+      )
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 })(CreateUserForm);
