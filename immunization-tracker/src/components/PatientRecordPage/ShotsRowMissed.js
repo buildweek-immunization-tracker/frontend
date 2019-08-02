209719de08242878737b04200d65369c6edf14a3
Trang Nguyen <ttn2127@columbia.edu>
2019-08-02 10:31:45 -0400

    removed provider column altogether

--- immunization-tracker/src/components/PatientRecordPage/ShotsRowMissed.js
+++ immunization-tracker/src/components/PatientRecordPage/ShotsRowMissed.js
@@ -6,7 +6,6 @@ export default function ShotsRowMissed({name, dose, location, dateReceived }){
         <Table.Row>
             <Table.Cell>{name}</Table.Cell>
             <Table.Cell>{dose}</Table.Cell>
-            <Table.Cell>{location}</Table.Cell>
             <Table.Cell textAlign='center'><Icon color='red' name='close icon' size='large' />{dateReceived}</Table.Cell>
         </Table.Row>
     )
--- immunization-tracker/src/components/ProviderRecordPage/ShotsHeaderP.js
+++ immunization-tracker/src/components/ProviderRecordPage/ShotsHeaderP.js
@@ -9,7 +9,6 @@ export default function ShotsHeaderP({type}){
             <Table.Row>
                 <Table.HeaderCell rowSpan='2'>Name</Table.HeaderCell>
                 <Table.HeaderCell rowSpan='2'>Dose</Table.HeaderCell>
-                <Table.HeaderCell rowSpan='2'>Provider</Table.HeaderCell>
                 <Table.HeaderCell colSpan='1'>Status</Table.HeaderCell>
                 <Table.HeaderCell colSpan='1'>Update</Table.HeaderCell>
             </Table.Row>
